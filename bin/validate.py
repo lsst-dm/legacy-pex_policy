c13e189483370abc1108688417f0dfa6376436bb
bbaker <bbaker@git.lsstcorp.org>
2009-07-31 23:17:06 +0000

    placeholder

---
+++ bin/validate.py
@@ -0,0 +1,10 @@
+#! /bin/python
+
+import pdb
+import unittest
+import math
+
+import lsst.utils.tests as tests
+from lsst.pex.policy import Policy, PolicyFile, Dictionary, ValidationError
+from lsst.pex.exceptions import LsstCppException
+

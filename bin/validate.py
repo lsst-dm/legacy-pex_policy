b72e23b40af033a0108af62887543ffc784eed53
bbaker <bbaker@git.lsstcorp.org>
2009-09-18 20:07:42 +0000

    fix perms

---
+++ bin/validate.py
@@ -0,0 +1,12 @@
+#! /bin/python
+
+import pdb
+import unittest
+import math
+
+import lsst.utils.tests as tests
+from lsst.pex.policy import Policy, PolicyFile, Dictionary, ValidationError
+from lsst.pex.exceptions import LsstCppException
+
+if __name__ == "__main__":
+    print "Validating.  Well eventually."

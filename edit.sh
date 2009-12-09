cb3db47bd71ffedbc3bcaf82817d56f367a1e1c0
bbaker <bbaker@git.lsstcorp.org>
2009-12-09 16:06:59 +0000

    add swig files

--- edit.sh
+++ edit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-emacs `find . -name "*.cc" -o -name "*.h" -o -name "*.py" -o -name "*.paf" -o -name "*.table"`
+emacs `find . -name "*.cc" -o -name "*.h" -o -name "*.py" -o -name "*.paf" -o -name "*.i" -o -name "*.table"`

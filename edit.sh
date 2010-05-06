59c38c93bd585d7365c4398064cfdbae0b3e2720
bbaker <bbaker@git.lsstcorp.org>
2010-05-06 16:06:32 +0000

    branch for ticket 1253

--- edit.sh
+++ edit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-emacs `find . \( -path "*tests/.test*" \) -prune -o -name "*.cc" -o -name "*.h" -o -name "*.py" -o -name "*.paf" -o -name "*.i" -o -name "*.table"`
+emacs `find . \( -path "*tests/.test*" \) -prune -o -name "*.cc" -o -name "*.h" -o -name "*.py" -o -name "*.paf" -o -name "*.i" -o -name "*.table" -o -name "*.dox"`

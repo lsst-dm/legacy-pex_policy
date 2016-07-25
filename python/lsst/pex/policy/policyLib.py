90622292741dd77825c775f5c059e3384e6a9e48
Pim Schellart <P.Schellart@princeton.edu>
2016-07-25 16:37:48 -0400

    Wrap with pybind11 instead of swig

---
+++ python/lsst/pex/policy/policyLib.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+import lsst.pex.exceptions
+
+from ._policySource import *
+from ._supportedFormats import *
+from ._policyString import *
+from ._policyStringDestination import *
+from ._policyFile import *
+from ._defaultPolicyFile import *
+from .policy import *
+from ._dictionary import *
+from ._urnPolicyFile import *
+from .paf import *
